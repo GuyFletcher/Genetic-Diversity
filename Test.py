934d05c9840352297127c2059b49c113db3d0cb2
Fletcher <fletcher.hart@mymail.champlain.edu>
2018-02-03 12:26:45 -0500

    initialization issue

--- Test.py
+++ Test.py
@@ -5,6 +5,9 @@ import random
 
 '''
 To generate random names = ''.join(random.choices(string.ascii_uppercase, k=5)) 
+
+#t = Tree("(A:1,(B:1,(E:1,D:1):0.5):0.5);" )
+#print (t)
 '''
 
 class Disease:
@@ -30,11 +33,27 @@ class Chromosome:
     active/inactive: Boolean
     Disease link: Disease object
     '''
-    name = "N/A"
-    is_xy = False
-    genes = [(None, [])] * 10
+    def __init__(self, entered_gene_size):
+        self.name = "N/A"
+        self.is_y = False
+        self.genes = [(None, [])] * 10
+        gene_size = entered_gene_size
+            
+        #instantiates the nucleobases of a gene and determines link with disease, if applicable.
+        for y in range (0, len(self.genes)):
+            acid_list = []
+            for x in range(0, gene_size):
+                acid_list.append(self.gene_name_generator())
+            
+            if random.randint(0,100) > 95:  #chance of gene linked disease set to less than 5%
+                disease = Disease()
+                disease.name = "Syndrome"
+                disease.gene_linked = True
+                self.genes[y] = (disease, acid_list)
+            else:
+                self.genes[y] = (None, acid_list)
     
-    def gene_name_generator():
+    def gene_name_generator(self):
         n_acid = random.randint(0, 3) 
         if n_acid == 0:
             return "A"
@@ -45,24 +64,11 @@ class Chromosome:
         else:
             return "G"       
         
-    gene_size = 10
-    
-    #instantiates the nucleobases of a gene and determines link with disease, if applicable.
-    for y in range (0, len(genes)):
-        acid_list = []
-        for x in range(0, gene_size):
-            acid_list.append(gene_name_generator())
-        
-        if random.randint(0,100) > 95:  #chance of gene linked disease set to less than 5%
-            disease = Disease()
-            disease.name = "Syndrome"
-            disease.gene_linked = True
-            genes[y] = (disease, acid_list)
-        else:
-            genes[y] = (None, acid_list)
+
+
             
 
-class Person(Chromosome):
+class Person:
     '''
     Class contains info on individuals. 
     Will contain variables for:
@@ -78,40 +84,57 @@ class Person(Chromosome):
         self.chromosomes = [None] * 20
     
     
-    def initialize(self, person):
+    '''def initialize(self):
         
         for x in range (0,20):
-            person.chromosomes[x] = Chromosome()
-            
-        person.chromosomes[18].is_xy = True
-        person.chromosomes[19].is_xy = True
-        print(person.chromosomes[0].genes)
+            self.chromosomes[x] = Chromosome()
+        
+        if random.randint(0,1) == 0:
+            self.chromosomes[19].is_y = True
+        else:
+            pass'''
+        #print(person.chromosomes[0].genes)
    
     
     
-def mate():
-    person1 = Person("Parent 1")
-    person2 = Person("Parent 2")
-    child = Person("Child")
+def mate():    
     
-    person1.initialize(person1)
-    person2.initialize(person2)
-    
-    for x in range (0, 20):
-        if random.randint(0,1) == 0:
-            child.chromosomes[x] = person1.chromosomes[x]
-        else:
-            child.chromosomes[x] = person2.chromosomes[x]
-    
-    print(person1.name, person1.chromosomes[0].genes)
-    print(person2.name, person2.chromosomes[0].genes)
-    print("Child", child.chromosomes[0].genes)
-    #print (gene_list[0].name) 
-    
-mate()
+    '''
+    if person1.chromosomes[19].is_y == True and person2.chromosomes[19].is_y == False:
+        print("Mating occurred")
+        for x in range (0, 20):
+            if random.randint(0,1) == 0:
+                child.chromosomes[x] = person1.chromosomes[x]
+            else:
+                child.chromosomes[x] = person2.chromosomes[x]
+        print(person1.name, person1.chromosomes[0].genes)
+        print(person2.name, person2.chromosomes[0].genes)
+        print("Child", child.chromosomes[0].genes)
+    else:
+        print("Both males, no mating occurred")
+    '''
 
-#t = Tree("(A:1,(B:1,(E:1,D:1):0.5):0.5);" )
-#print (t)
+def initialize(person):
+    
+    for x in range (0,20):
+        person.chromosomes[x] = Chromosome(10)
+    
+    if random.randint(0,1) == 0:
+        person.chromosomes[19].is_y = True
+    else:
+        pass    
+
+parent1 = Person("Parent 1")
+parent2 = Person("Parent 2")
+child = Person("Child")   
+
+initialize(parent1)    
+initialize(parent2)
+
+print(parent1.name, parent1.chromosomes[0].genes)
+print(parent2.name, parent2.chromosomes[0].genes)
+
+mate()
 
 
 
